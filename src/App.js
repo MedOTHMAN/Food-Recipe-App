0afde2708b57b642fcf84d850d7a9c53b8c11ec9
MedOTHMAN <mohamed.n.othman@gmail.com>
2021-06-28 22:40:37 +0100

    V1.0.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import {Header,AppNameComponent,AppIcon,SearchComponent,SearchIcon,SearchInput } from "./components/headerComponent";
+import {RecipeListContainer,RecipeContainer,CoverImage,RecipeName,IngredientsText,SeeMoreText } from "./components/recipeComponent";
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import React from "react";
+
+
+const APP_ID = "f429e466";
+const APP_KEY = "204f48ccc5874fbb6078e3470c2069a5";
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const RecipeComponent = (props) => {
+  console.log(props);
+  const {recipeObj} = props;
+  const [show, setShow] = React.useState(false);
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return(
+    <>
+    <Dialog open={show} onClose={handleClose}>
+    <DialogTitle id="alert-dialog-slide-title">Ingredient</DialogTitle>
+    <DialogContent>
+      <table>
+        <thead>
+          <th>Ingredients</th>
+          <th>Weight</th>
+        </thead>
+        <tbody>
+          {recipeObj.ingredients.map((ingredientObj)=> (
+          <tr>
+            <td>{ingredientObj.text}</td>
+            <td>{ingredientObj.weight}</td>
+          </tr>
+
+          ))}
+        </tbody>
+      </table>
+    </DialogContent>
+    <DialogActions>
+          <SeeNewTab onClick={() => window.open(url)}>See More</SeeNewTab>
+          <SeeMoreText onClick={()=> setShow("")}>Close</SeeMoreText>
+        </DialogActions>
+     </Dialog>
+    <RecipeContainer>
+            <CoverImage src={recipeObj.image} />
+            <RecipeName>{recipeObj.label}</RecipeName>
+            <IngredientsText onClick={() => setShow(true)}>ingredients</IngredientsText>
+            <SeeMoreText onClick={() => window.open(recipeObj.url)}>See Complete Recipe</SeeMoreText>
+          </RecipeContainer>
+          </>
+  )
+}
 
 function App() {
+
+  const [timeoutId, updateTimeoutId] = useState();
+  const [recipeList, updateRecipeList] = useState([]);
+
+  const fetchRecipe = async (searchString) =>{
+    const response = await Axios.get(`https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    updateRecipeList(response.data.hits);
+  };
+
+  const onTextChange= (event) => {
+    clearTimeout(timeoutId);
+    const timeout= setTimeout(()=> fetchRecipe(event.target.value),500);
+    updateTimeoutId(timeout)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppNameComponent><AppIcon src="/hamburger.svg" />Recipe Finder</AppNameComponent>
+        <SearchComponent>
+          <SearchIcon src="/search-icon.svg" />
+          <SearchInput placeholder="Search Recipe" onChange={onTextChange} />
+        </SearchComponent>
+        </Header>
+        <RecipeListContainer>
+          {recipeList.length===0 ? "Taper un recipe" :  recipeList.length && recipeList.map((recipeObj) => (
+            <RecipeComponent recipeObj={recipeObj.recipe} />
+          ))}
+        </RecipeListContainer>
+    </Container>
   );
 }
 
